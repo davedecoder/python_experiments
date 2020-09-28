718ac9aa5561ee0b955fed06cabe1045ddc09861
David I.Rock <entrinen@gmail.com>
2020-09-27 20:50:26 -0500

    Adding sheapq priority queue

---
+++ scripts/priority_heapq_queues.py
@@ -0,0 +1,11 @@
+import heapq
+
+q = []
+
+heapq.heappush(q, (2, 'code'))
+heapq.heappush(q, (1, 'eat'))
+heapq.heappush(q, (3, 'sleep'))
+
+while q:
+    next_item = heapq.heappop(q)
+    print(next_item)
